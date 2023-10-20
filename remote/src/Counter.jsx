de811f6a9f79aea233ee4cc03d052cea365e22c9
Carlos Ferreira <carlosferreirahd@gmail.com>
2023-10-20 20:57:07 -0300

    refactor[remote]: adding text prop to Counter to test props passing

--- remote/src/Counter.jsx
+++ remote/src/Counter.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 
 import "./index.css";
 
-export function Counter() {
+export function Counter({
+  from = "",
+}) {
 
   const [count, setCount] = useState(0);
 
   return (
-    <div className='remote-counter'>
+    <div>
+      <p>from: {from}</p>
       <p>count: {count}</p>
       <button onClick={() => setCount(currCount => currCount + 1)}>
         increase
